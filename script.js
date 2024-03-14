858c5c3d062d1a11ed0504fef110048c95cd2fa0
thedevanshagrawal <thedevanshagrawal@gmail.com>
2024-03-14 20:05:28 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// const options = {
-//     mathod: 'GET',
-//     headers: {
-//         'X-RAPIDAPI-Key': 'b45e96323fmshf5e4bc50d3b8a90p1e091djsnfb21cde4eb87',
-//         'X-RAPIDAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-//     }
-// }
-
-// fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-//     .then(response => response.json())
-//     .then((response) => {
-//         console.log(response)
-
-//     })
-//     .catch(err => console.error(err))
-
 const inputBox = document.querySelector(".input-box")
 const searchBtn = document.getElementById("searchBtn")
 const weather_img = document.querySelector(".weather-image")
@@ -23,9 +7,10 @@ const humidity = document.querySelector("#humidity")
 const wind_speed = document.querySelector("#wind-speed")
 const location_not_found = document.querySelector(".location-not-found")
 const weather_body = document.querySelector(".weather-body")
+const api_key = "4d8d0c84a16dc2d6b3f5de3fad40e1a8"
 
 async function checkWeather(city) {
-    const api_key = "4d8d0c84a16dc2d6b3f5de3fad40e1a8"
+
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
 
     const weatherData = await fetch(`${url}`).then(res => res.json())
